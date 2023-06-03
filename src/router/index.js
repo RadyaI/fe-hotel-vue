13ee63803de4a809f6d0184180e99ea53cd79804
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-06-03 10:24:51 +0700

    Feedback

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+// public
 import DashboardPublic from '../views/public/Dashboard.vue'
+import Check from '../views/public/Check.vue'
+import Feedback from '../views/public/Feedback.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // =======> Public Route <=======
   {
     path: '/',
     component: DashboardPublic
+  },
+  {
+    path: '/check',
+    component: Check
+  },
+  {
+    path: '/feedback',
+    component: Feedback
   }
+  // =======> Public Route <=======
 ]
 
 const router = new VueRouter({
